81d6d93925ed6c7734def8dcec810466cf5cec91
VictorNaumov <anakinsanakins@outlook.com>
2021-10-28 21:11:33 +0300

    Changes description and declaration methods

--- AuthenticationService/Controllers/AccountController.cs
+++ AuthenticationService/Controllers/AccountController.cs
@@ -13,33 +13,33 @@ namespace AuthenticationService.Controllers
         public AccountController(IMediator _mediator) : base(_mediator)
         { }
 
-        [HttpPost("register")]
         /// <summary> Create a new user account </summary>
         /// <params name="registerUser"></param>
+        [HttpPost("register")]
         public async Task<IActionResult> RegisterUser([FromBody] RegistrationUserDto registerUser,
             CancellationToken cancellationToken) =>
             await ExecuteCommandAsync(new RegisterUserCommand(registerUser), cancellationToken: cancellationToken);
 
-        [HttpPost("authenticate")]
         /// <summary> Authenticate and autorization user if his exists in the database </summary>
         /// <param name="authenticateUser"></param>
         /// <returns> Bearer token with user roles </returns>
+        [HttpPost("authenticate")]
         public async Task<IActionResult> AuthenticateUser([FromBody] AuthenticationUserDto authenticateUser,
             CancellationToken cancellationToken) =>
             await ExecuteQueryAsync(new AuthenticateUserQuery(authenticateUser), cancellationToken: cancellationToken);
 
-        [HttpPut("change-password")]
         /// <summary> Change account password</summary>
         /// <param name="changePassword"></param>
         /// <returns> No content </returns>
+        [HttpPut("change-password")]
         public async Task<IActionResult> ChangeUserPassword([FromBody] ChangeUserPasswordDto changePassword,
             CancellationToken cancellationToken) =>
             await ExecuteCommandAsync(new ChangeUserPasswordCommand(changePassword), cancellationToken: cancellationToken);
 
-        [HttpDelete]
         /// <summary> Delete user account</summary>
         /// <param name="authenticateUser"></param>
         /// <returns> No content </returns>
+        [HttpDelete]
         public async Task<IActionResult> DeleteUser([FromBody] AuthenticationUserDto authenticateUser,
             CancellationToken cancellationToken) =>
             await ExecuteCommandAsync(new DeleteUserCommand(authenticateUser), cancellationToken: cancellationToken);
